84670ac2859d188ab4e06ad80329a1e811342d16
ahmed <ahmed@DESKTOP-LSUT3NK>
2025-04-14 23:48:23 +0200

    Specification DessignBattern

--- Core/Services/MapperReference.cs
+++ Core/Services/MapperReference.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Services
 {
+
+
+    // use this class to call object from IMapper Assebly (just class in same project)
     public class MapperReference
     {
     }
---
+++ Infrastructure/Persistance/SpecificationEvaluator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Contracts;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    internal class SpecificationEvaluator
+    {
+        // to Generate StaticQuery
+        public static IQueryable<TEntity> GetQuery<TEntity,TKey>(IQueryable<TEntity> InputQuery,ISpecification<TEntity,TKey> Spec) 
+
+            where TEntity : BaseEntity<TKey>
+        {
+            var query = InputQuery;
+            if(Spec.Criteria is not null)
+             query=  query.Where(Spec.Criteria);
+
+            query=   Spec.IncludeExpressions.Aggregate(query ,(currentQuery, includExperstion) => currentQuery.Include(includExperstion));
+
+            return query;
+        }
+
+    }
+}
