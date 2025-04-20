2b50390d65206df42f9beba691568e4c76b998cf
ahmed <ahmed@DESKTOP-LSUT3NK>
2025-04-20 02:06:24 +0200

    RefactoringMainFunctionAndGlobal

---
+++ Core/Services/ApplicationServicesRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Services.Abstraction;
+using Services.manger;
+
+namespace Services
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MapperReference).Assembly);
+            services.AddScoped<IServiceManger, ServiceManger>();
+
+            return services;
+        }
+
+    }
+}
---
+++ Infrastructure/Persistance/InfrastructureRegistrations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistance.Data.Contexts;
+using Persistance.Repositories;
+using Persistance.Seedingclass;
+
+namespace Persistance
+{
+    public static class InfrastructureRegistrations
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                //options.UseSqlServer("DefaultConnection");
+                //or
+                //options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]);
+                //or
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+           services.AddScoped<IDbIntializer, DbIntializer>();  //Allow DI to create object DbIntializer
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+
+    }
+}
